b8ff7e9f1c57af246350a5980f2f09e7b05b69bb
mhamilt <mhamilton942@gmail.com>
2018-11-02 14:16:20 +0000

    Update data/sine_wave.glsl

--- data/sine_wave.glsl
+++ data/sine_wave.glsl
@@ -12,14 +12,14 @@ uniform vec2 resolution;
 uniform vec2 mouse;
 //==============================================================================
 const vec3 color = vec3(.0, .9, .7);
-const float intensity = 1.1;
+const float intensity = 4.1;
 //==============================================================================
 float band(vec2 pos, float amplitude, float frequency)
 {
-	float wave = ((1.0 - (cos(twoPi * pos.x))) * 0.5) *(sin(time*2) * amplitude * cos(twoPi * frequency * pos.x + time) / 2.);
+	float wave = ((1.0 - (cos(twoPi * pos.x))) * 0.5) * (amplitude * cos(twoPi * frequency * pos.x + time) / 2.);
 	float colourScaling = 0.001;
-	float minValue = 0.0002;
-	float maxValue = 2.0;
+	float minValue = 0.001;
+	float maxValue = 4.0;
 	float wavePosition = clamp(abs(wave - pos.y + .5), 0.001, 2.) ; // cosine - pixel ycoord (0 < wavePosition < 1)
 	float light = clamp(amplitude * frequency * colourScaling, minValue, maxValue) / wavePosition; // this worked on zero division. this was a hack
 	return light * intensity;
